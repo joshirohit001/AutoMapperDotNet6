f9000ff443531e754132dc25f5d8ddb9e5d85cfe
Rohit Joshi <rj.rohitjoshi@hotmail.com>
2022-09-24 22:50:17 +0530

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCLearn.BAL.Interface;
+using MVCLearn.MAL;
+
+namespace MVCLearn.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployee _service;
+        public EmployeeController(IEmployee service)
+        {
+            _service = service;
+        }
+
+        #region Get the list of Employee
+        /// <summary>
+        /// Get the list of Employee
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            List<EmployeeModel> finalResult = new();
+            try
+            {
+                finalResult = _service.GetEmployees();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return View(finalResult);
+        }
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using MVCLearn.BAL.Interface;
+using MVCLearn.BAL.Service;
+using MVCLearn.Common;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IEmployee, EmployeeService>();
+var config = new MapperConfiguration(cfg =>
+{
+    cfg.AddProfile(new AutoMapperProfile());
+});
+builder.Services.AddSingleton(config.CreateMapper());
+var app = builder.Build();
+
+//Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    //The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
