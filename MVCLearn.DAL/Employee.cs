ab51c945bbce5e5165d107d5a29fdcc664cb3db7
joshirohit001 <88126081+joshirohit001@users.noreply.github.com>
2022-09-25 11:50:27 +0530

    Add files via upload

---
+++ MVCLearn.DAL/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace MVCLearn.DAL
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; } 
+        public string? LastName { get; set; }
+        public string? Country { get; set; }
+    }
+}
---
+++ MVCLearn.MAL/EmployeeModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MVCLearn.MAL
+{
+    public class EmployeeModel
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Country { get; set; }
+    }
+}
